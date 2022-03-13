cb5e24f5096e9515db97201306da20667fc8e281
Quentin JEANNINGROS <quentin.jeanningros@hotmail.fr>
2022-03-13 16:24:17 +0100

    Update css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,17 +3,25 @@ import styled from 'styled-components';
 import { A2LogoBlack } from '../Icons'
 
 const StyledFooterContainer = styled.footer`
-    height: 250px;
-
+    height: 192px;
     display: flex;
     flex-direction: row;
-    align-items: center;
-    justify-content: space-evenly;
-
+    align-items: top;
+    justify-content: space-between;
     background-color: ${props => props.theme.mainColor};
+    padding-top: 58px;
+`
+const StyledFooterContainerText = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 72%;
+    margin-right: 55px; 
+    flex-direction: row;
 `
 
-const StyledFooterContainerItemIcon = styled.div``
+const StyledFooterContainerItemIcon = styled.div`
+    margin-left: 44px;
+`
 const StyledFooterContainerItemContacts = styled.div`
     display: flex;
     flex-direction: column;
@@ -28,7 +36,7 @@ const StyledFooterContainerItemSocialNetworks = styled.div`
 const StyledFooterContainerItemDevsCopyrights = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 60px;
+    gap: 88px;
 `
 
 const StyledFooterContainerItemDevs = styled.div`
@@ -40,6 +48,7 @@ const StyledFooterContainerItemDevs = styled.div`
 const StyledLink = styled.a`
     font-size: 15px;
     text-decoration: none;
+    color: #040205;
     &:hover {
         cursor: pointer;
     }
@@ -60,24 +69,26 @@ const Footer = () => {
             <StyledFooterContainerItemIcon onClick={() => {}}>
                 <A2LogoBlack width='114.59px' height='119px' />
             </StyledFooterContainerItemIcon>
-            <StyledFooterContainerItemContacts>
-                <StyledLink href='mailto:2moy.auxence@gmail.com'>2moy.auxence@gmail.com</StyledLink>
-                <StyledLink href='tel:+33605416548'>+33 6 05 41 65 48</StyledLink>
-            </StyledFooterContainerItemContacts>
-            <StyledFooterContainerItemSocialNetworks>
-                <StyledLink target='_blank' href='https://www.instagram.com/a2.lab/'>instagram</StyledLink>
-                <StyledLink target='_blank' href='https://dribbble.com/a2moy/'>dribble</StyledLink>
-                <StyledLink target='_blank' href='https://fr.linkedin.com/in/auxence-demoy-7baabb166/'>linkedin</StyledLink>
-            </StyledFooterContainerItemSocialNetworks>
-            <StyledFooterContainerItemDevsCopyrights>
-                <StyledFooterContainerItemDevs>
-                    <StyledText>designer: auxence demoy</StyledText>
-                    <StyledText>developers: <StyledLink href=''>quentin jeaningros</StyledLink> - <StyledLink href=''>nathan spanier</StyledLink></StyledText>
-                </StyledFooterContainerItemDevs>
-                <div>
-                    <StyledCopyrights>© 2022 - A2MOY - ALL RIGHTS RESERVED</StyledCopyrights>
-                </div>
-            </StyledFooterContainerItemDevsCopyrights>
+            <StyledFooterContainerText>
+                <StyledFooterContainerItemContacts>
+                    <StyledLink href='mailto:2moy.auxence@gmail.com'>2moy.auxence@gmail.com</StyledLink>
+                    <StyledLink href='tel:+33605416548'>+33 6 05 41 65 48</StyledLink>
+                </StyledFooterContainerItemContacts>
+                <StyledFooterContainerItemSocialNetworks>
+                    <StyledLink target='_blank' href='https://www.instagram.com/a2.lab/'>instagram</StyledLink>
+                    <StyledLink target='_blank' href='https://dribbble.com/a2moy/'>dribble</StyledLink>
+                    <StyledLink target='_blank' href='https://fr.linkedin.com/in/auxence-demoy-7baabb166/'>linkedin</StyledLink>
+                </StyledFooterContainerItemSocialNetworks>
+                <StyledFooterContainerItemDevsCopyrights>
+                    <StyledFooterContainerItemDevs>
+                        <StyledText>designer: auxence demoy</StyledText>
+                        <StyledText>developers: <StyledLink href=''>quentin jeaningros</StyledLink> - <StyledLink href=''>nathan spanier</StyledLink></StyledText>
+                    </StyledFooterContainerItemDevs>
+                    <div>
+                        <StyledCopyrights>© 2022 - A2MOY - ALL RIGHTS RESERVED</StyledCopyrights>
+                    </div>
+                </StyledFooterContainerItemDevsCopyrights>
+            </StyledFooterContainerText>
         </StyledFooterContainer>
     )
 }
