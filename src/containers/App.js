bdce2c1ee544332784e06a90d052bbef0dd60f35
Quentin JEANNINGROS <quentin.jeanningros@hotmail.fr>
2022-04-09 16:40:53 +0200

    add animation

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import styled from 'styled-components';
-
 import { Footer, Navbar } from '../components';
 import { ProjectsPage, AboutPage, ContactPage, HomePage, PageNotFound, ProjectPage } from ".";
 import { ThemeProvider } from "styled-components";
@@ -24,20 +22,6 @@ const App = () => {
     const [currentTheme, setCurrentTheme] = useState(themes.red);
     return (
         <ThemeProvider theme={currentTheme}>
-<<<<<<< HEAD
-            <AppContainer>
-            <Navbar setCurrentTheme={setCurrentTheme}/>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="projects" element={<ProjectsPage />} />
-                <Route path="projects/:projectId" element={<ProjectPage />} />
-                <Route path="about" element={<AboutPage />} />
-                <Route path="contact" element={<ContactPage />} />
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-            <Footer />
-            </AppContainer>
-=======
             <Root>
                 <Navbar setCurrentTheme={setCurrentTheme} />
                 <Routes>
@@ -50,7 +34,6 @@ const App = () => {
                 </Routes>
                 <Footer />
             </Root>
->>>>>>> e24e7251bf007189d4652f50c7a060f7b91c3262
         </ThemeProvider>
     );
 }
