9352798ac64eebb887fa647b8f005444a9c9c8b0
Quentin JEANNINGROS <quentin.jeanningros@hotmail.fr>
2022-04-10 02:14:31 +0200

    3 column issue fix

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -14,16 +14,12 @@ const StyledNavbarContainer = styled.nav`
 const StyledTopNavbar = styled.div`
     background-color: ${props => props.theme.mainColor};
     width: inherit;
-    display: flex;
     align-items: center;
-    justify-content: space-between;
     padding: 25px;
-    @media(max-width: 767px) {
-        flex-direction: row-reverse;
-    }
 `
 
 const StyledBottomNavbar = styled.div`
+    padding: 0px 25px;
     height: 25px;
     width: inherit;
     display: flex;
@@ -34,7 +30,12 @@ const StyledElementSquareContainer = styled.div`
     display: flex;
     flex-direction: row;
     align-items: flex-start;
-    flex-basis: 60%;
+`
+
+const Logo = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
 `
 
 const StyledElementSquareWhite = styled.div`
@@ -54,7 +55,6 @@ const StyledElementSquareBlack = styled.div`
 `
 
 const StyledText = styled.span`
-    font-style: normal;
     font-weight: bold;
     font-size: 15px;
     line-height: 18px;
@@ -66,7 +66,6 @@ const StyledNavigationButton = styled(Link)`
     font-size: 15px;
     line-height: 18px;
     color: #040205;
-    font-style: normal;
     font-weight: bold;
     @media(max-width: 767px) {
         margin: 4.5px;
@@ -113,8 +112,6 @@ const ColorElement = styled.span`
 const StyledButton = styled.span`
     align-self: center;
     text-decoration: none;
-    font-style: normal;
-    font-weight: bold;
     font-size: 15px;
     color: #A6A4AB;
     transition: 0.1s;
@@ -134,17 +131,16 @@ const StyledColorSelector = styled.div`
     height: 25px;
 `
 
-const EmptyBox = styled.div`
-    width: 22.7%;
-    height: 10px;
-    min-width: 285px;
+const Title = styled.div`
+    display: flex;
+    justify-content: space-between;
 `
 
 const ColorSelector = ({ setCurrentTheme }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [isActive, setIsActive] = useState("red");
     return (
-        <StyledColorSelector className='the-column' >
+        <StyledColorSelector className='column-2'>
             <StyledButton to="/">the lab</StyledButton>
             <ColorElementsContainer isOpen={isOpen}>
                 <StyledButton onClick={() => { setIsOpen(!isOpen) }}>change color</StyledButton>
@@ -176,21 +172,24 @@ const ColorSelector = ({ setCurrentTheme }) => {
 const Navbar = ({ setCurrentTheme }) => {
     return (
         <StyledNavbarContainer>
-            <StyledTopNavbar className="neue-montreal-bold">
-                <StyledTitle>AUXENCE DEMOY</StyledTitle>
-                <StyledTitle>DIGITAL DESIGNER & ART DIRECTOR</StyledTitle>
-                <EmptyBox/>
-                <StyledNavigationContainer className='the-column'>
+            <StyledTopNavbar className='columns-container'>
+                <Title className='column-1'>
+                    <StyledTitle>AUXENCE DEMOY</StyledTitle>
+                    <StyledTitle>DIGITAL DESIGNER &#38; ART DIRECTOR</StyledTitle>
+                    <span/>
+                </Title>
+                <StyledNavigationContainer className='column-2'>
                     <StyledNavigationButton to='/'>HOME</StyledNavigationButton>
                     <StyledNavigationButton to='/projects'>PROJECT</StyledNavigationButton>
                     <StyledNavigationButton to='/about'>ABOUT</StyledNavigationButton>
                     <StyledNavigationButton to='/contact'>CONTACT</StyledNavigationButton>
                 </StyledNavigationContainer>
-                <A2LogoBlack width='18px' height='18px' />
+                <Logo className='column-3'>
+                    <A2LogoBlack width='18px' height='18px' />
+                </Logo>
             </StyledTopNavbar>
-            <StyledBottomNavbar>
-                <StyledElementSquareContainer>
-                    <StyledElementSquareWhite />
+            <StyledBottomNavbar className='columns-container'>
+                <StyledElementSquareContainer className='column-1'>
                     <StyledElementSquareBlack />
                     <StyledElementSquareWhite />
                     <StyledElementSquareBlack />
@@ -198,6 +197,7 @@ const Navbar = ({ setCurrentTheme }) => {
                     <StyledElementSquareBlack />
                 </StyledElementSquareContainer>
                 <ColorSelector setCurrentTheme={setCurrentTheme}/>
+                <span className='column-3'/>
             </StyledBottomNavbar>
         </StyledNavbarContainer>
     )
--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,15 +1,39 @@
 import styled from 'styled-components';
 
-const Content = styled.div`
-    padding: 0 25px;
-    height: 500px;
+const Intro = styled.div`
+    padding: 90px 25px;
+    width: inherit;
+    align-items: flex-start;
+`
+
+const Title = styled.div`
+    font-size: 72px;
+    font-weight: bold;
+    color: ${props => props.theme.mainColor};
+`
+
+const Paragraph = styled.div`
+    font-size: 18px;
+    text-align: justify;
+`
+
+const TitleContainer = styled.div`
+    width: 100%;
 `
 
 const HomePage = () => {
     return (
-        <Content>
-            HomePage
-        </Content>
+        <div>
+            <Intro className='columns-container'>
+                <TitleContainer className='column-1'>
+                    <Title>DIGITAL DESIGNER &#38; ART DIRECTOR</Title>
+                </TitleContainer>
+                <Paragraph className='column-2'>
+                    focus on digital products, visual identity and art direction, to help companies, brands and entrepreneurs develop and achieve their goals.
+                </Paragraph>
+                <span className='column-3'/>
+            </Intro>
+        </div>
     );
 }
 
