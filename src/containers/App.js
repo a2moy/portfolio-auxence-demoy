ec05babdad473b66fe61846977ee892cae717496
Quentin JEANNINGROS <quentin.jeanningros@hotmail.fr>
2022-04-12 22:43:00 +0200

    add page transition animation

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, createRef } from "react";
 import { Routes, Route } from "react-router-dom";
 import styled from 'styled-components';
+import anime from 'animejs/lib/anime.es.js';
 
 import { Footer, Navbar } from '../components';
 import { ProjectsPage, AboutPage, ContactPage, HomePage, PageNotFound, ProjectPage } from ".";
@@ -14,33 +15,57 @@ const Root = styled.div`
     z-index: 1;
 `
 
-/*const PageTransition = styled.div`
+const PageTransition = styled.div`
     position: fixed;
-    height: 100vh;
+    height: 0vh;
     width: 100vw;
-    top: 0px;
+    bottom: 0px;
     left: 0px;
     background-color: ${props => props.theme.mainColor};
-    z-index: 999;
-`*/
+    z-index: 990;
+`
 
 const App = () => {
     const [currentTheme, setCurrentTheme] = useState(themes.red);
 
-    const InAnimationStart = () => {}
-    //const InAnimationEnd = () => {}
+    const transisitonDivRef = createRef();
+
+    const pageTransitionAnimationIn = () => {
+        anime({
+            targets: transisitonDivRef.current,
+            height: '100vh',
+            duration: 500,
+            easing: 'easeOutCubic',
+            complete: (anim) => {
+                StartOutAnimation()
+            },
+        });
+    }
+
+    const pageTransitionAnimationOut = () => {
+        anime({
+            targets: transisitonDivRef.current,
+            height: '0vh',
+            duration: 500,
+            easing: 'easeInCubic',
+        });
+    }
+
+    const StartInAnimation = () => {pageTransitionAnimationIn()}
+    const StartOutAnimation = () => {pageTransitionAnimationOut()}
 
     return (
         <ThemeProvider theme={currentTheme}>
             <Root>
+                <PageTransition ref={transisitonDivRef}/>
                 <Navbar setCurrentTheme={setCurrentTheme} />
                 <Routes>
-                    <Route path="/" element={<HomePage                          mountCallback={() => {}} unmountCallback={InAnimationStart}/>}/>
-                    <Route path="projects" element={<ProjectsPage               mountCallback={() => {}} unmountCallback={InAnimationStart}/>} />
-                    <Route path="projects/:projectId" element={<ProjectPage     mountCallback={() => {}} unmountCallback={InAnimationStart}/>} />
-                    <Route path="about" element={<AboutPage                     mountCallback={() => {}} unmountCallback={InAnimationStart}/>} />
-                    <Route path="contact" element={<ContactPage                 mountCallback={() => {}} unmountCallback={InAnimationStart}/>} />
-                    <Route path="*" element={<PageNotFound                      mountCallback={() => {}} unmountCallback={InAnimationStart}/>} />
+                    <Route path="/" element={<HomePage                          mountCallback={() => {}} unmountCallback={StartInAnimation}/>}/>
+                    <Route path="projects" element={<ProjectsPage               mountCallback={() => {}} unmountCallback={StartInAnimation}/>} />
+                    <Route path="projects/:projectId" element={<ProjectPage     mountCallback={() => {}} unmountCallback={StartInAnimation}/>} />
+                    <Route path="about" element={<AboutPage                     mountCallback={() => {}} unmountCallback={StartInAnimation}/>} />
+                    <Route path="contact" element={<ContactPage                 mountCallback={() => {}} unmountCallback={StartInAnimation}/>} />
+                    <Route path="*" element={<PageNotFound                      mountCallback={() => {}} unmountCallback={StartInAnimation}/>} />
                 </Routes>
                 <Footer />
             </Root>
